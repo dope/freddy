94ef1fc25260a5181b02ff567507648b0a8cb8cb
Joe <joe@lifes.gd>
2017-12-21 01:53:19 +0000

    feat(tree) add highlighting (#2)

--- src/renderer/components/Editor.vue
+++ src/renderer/components/Editor.vue
@@ -15,7 +15,6 @@
   require('codemirror/addon/lint/lint.css')
   require('codemirror/addon/lint/lint.js')
   require('codemirror/addon/lint/json-lint.js')
-  require('codemirror/theme/duotone-dark.css')
 
   export default {
     name: 'Editor',
@@ -30,7 +29,6 @@
         editorOptions: {
           tabSize: 2,
           mode: 'application/ld+json',
-          theme: 'duotone-dark',
           lineNumbers: true,
           line: true,
           lineWrapping: true,
@@ -78,14 +76,13 @@
   .Editor-Input {
     position: relative;
     width: 100%;
-    border-right: 1px solid #1C1924;
+    border-right: 1px solid rgba(black, 0.8);
   }
 
   .Editor-Output {
     width: 100%;
     padding: 10px;
-    background: #23202C;
-    color: rgba(225, 225, 225, 0.7);
+    background: #211E29;
     line-height: 1.7;
     padding: 35px;
     -webkit-font-smoothing: antialiased;
@@ -106,28 +103,80 @@
   }
 
   .tree-view-item-value-string {
-    white-space: pre-wrap !important;
-  }
-
-  .tree-view-item-key-with-chevron {
-    color: #B5B2BD;
-  }
-
-  .tree-view-item-key {
-    color: #777180;
-  }
-
-  .tree-view-item-hint,
-  .tree-view-item-value-string {
-    color: #B9B5C3 !important;
+    white-space: pre-wrap;
   }
 
   .CodeMirror-lint-tooltip {
-    background: #211E29 !important;
+    background: #211E29;
     color: #FFF;
     padding: 10px;
     border: 0;
     border-left: 5px solid lighten(#211E29, 20%);
     border-radius: 0;
+  }
+
+  .tree-view-item-leaf {
+    position: relative;
+
+    &:after {
+      top: 0;
+      left: -13px;
+      position: absolute;
+      width: 1px;
+      height: 100%;
+      background: lighten(#211E29, 10%);
+      content: "";
+    }
+  }
+
+  .CodeMirror-gutters,
+  .CodeMirror {
+    background: #292533;
+    line-height: 1.5;
+  }
+
+  .CodeMirror-gutters {
+    border-right: 0;
+  }
+
+  .CodeMirror-linenumber {
+    color: rgba(white, 0.2) !important;
+  }
+
+  .CodeMirror-line {
+    color: #5B5765 !important;
+  }
+
+  .cm-property {
+    color: #868290 !important;
+  }
+
+  .cs-number,
+  .cm-atom,
+  .cm-string {
+    color: #ACA8B6 !important;
+  }
+
+  .cm-s-default .cm-number {
+    color: #ACA8B6 !important;
+  }
+
+  .tree-view-item-key {
+    color: #B1ADC3;
+    font-weight: 300;
+  }
+
+  .tree-view-item-key-with-chevron {
+    color: #9F8DE5;
+    font-weight: 600 !important;
+  }
+
+  .tree-view-item-leaf {
+    color: #9187BA;
+  }
+
+  .tree-view-item-value-number,
+  .tree-view-item-value-string {
+    color: #fec38f
   }
 </style>
